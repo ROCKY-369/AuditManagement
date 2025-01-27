ea2d4ad2cef6a616faeff41ea06d6a1606bd6068
Bhanu Siva Kumar Kommana <bsivakumar369@gmail.com>
2025-01-27 16:32:32 -0500

    Delete Audit Management System/Audit Management directory

--- Audit Management System/Audit Management/Audit-Benchmark/src/main/java/com/cognizant/benchmark/exception/InvalidTokenException.java
+++
@@ -1,9 +0,0 @@
-package com.cognizant.benchmark.exception;
-
-public class InvalidTokenException extends RuntimeException{
-	
-	public InvalidTokenException(String message){
-		super(message);
-	}
-
-}
