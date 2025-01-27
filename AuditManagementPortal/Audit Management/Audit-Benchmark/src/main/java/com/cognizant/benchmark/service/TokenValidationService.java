774c43b6ab96560b89d4ff46b5e5c133894efdc2
Bhanu Siva Kumar Kommana <bsivakumar369@gmail.com>
2025-01-27 16:54:33 -0500

    Delete AuditManagementPortal/Audit Management directory

--- AuditManagementPortal/Audit Management/Audit-Benchmark/src/main/java/com/cognizant/benchmark/service/TokenValidationService.java
+++
@@ -1,7 +0,0 @@
-package com.cognizant.benchmark.service;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface TokenValidationService {
-    public boolean checkValidity(String token);
-}
